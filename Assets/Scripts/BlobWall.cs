b6d1122dc453b9c450c4d61771c85f8edfeb29f4
Valentin CLIMPONT <valentin.climpont@gmail.com>
2021-04-23 21:33:15 +0200

    implemented blob walls

---
+++ Assets/Scripts/BlobWall.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobWall : MonoBehaviour
+{
+    public int side;
+    public float lifeTime;
+
+    private float targetX;
+    private float startX;
+
+    private Coroutine cMoveTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startX = transform.position.x;
+
+        if (side == 0)
+        {
+            targetX = transform.position.x + 4f;
+        }
+        else
+        {
+            targetX = transform.position.x - 4f;
+            transform.rotation = Quaternion.Euler(0, 0, 180f);
+        }
+
+        StartCoroutine(DestroyAfterLifeTime());
+        cMoveTo = StartCoroutine(CMoveTo(targetX, 3f));
+    }
+
+    IEnumerator CMoveTo(float x, float moveTime, bool destroy = false)
+    {
+        Vector3 startPosition = transform.position;
+        float dt = 0;
+
+        while (dt <= moveTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, new Vector3(x, startPosition.y, startPosition.z), dt / moveTime);
+
+            dt += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = new Vector3(x, startPosition.y, startPosition.z);
+
+        if (destroy)
+            Destroy(gameObject);
+
+        cMoveTo = null;
+        yield return null;
+    }
+     
+    IEnumerator DestroyAfterLifeTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+
+        if(cMoveTo != null)
+        {
+            StopCoroutine(cMoveTo);
+        }
+
+        cMoveTo = StartCoroutine(CMoveTo(startX, 2f, true));
+    }
+
+    void OnDestroy()
+    {
+        if(WallSpawner.SpawnPositionOnYAxis.Contains(transform.position.y))
+        {
+            WallSpawner.SpawnPositionOnYAxis.Remove(transform.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject blobWallPrefab = null;
+    [SerializeField] private Transform anchorLeft = null;
+    [SerializeField] private Transform anchorRight = null;
+    [SerializeField] private Transform anchorBotLeft = null;
+    [SerializeField] private Transform anchorBotRight = null;
+
+    public float timeBeforeSpawningFirstWall = 0f;
+    public float spawnMinRate = 5f;
+    public float spawnMaxRate = 20f;
+    public float wallLifeTimeMin = 7f;
+    public float wallLifeTimeMax = 12f;
+
+    public static List<float> SpawnPositionOnYAxis { get; set; }
+
+    void Awake()
+    {
+        SpawnPositionOnYAxis = new List<float>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CSpawnWalls());
+    }
+
+    private IEnumerator CSpawnWalls()
+    {
+        yield return new WaitForSeconds(timeBeforeSpawningFirstWall);
+
+        while (true)
+        {
+            int s = Random.Range(0, 2); // rand between 0 1
+            float x = s == 0 ? Random.Range(anchorLeft.position.x - 5f, anchorLeft.position.x - 1f) : Random.Range(anchorRight.position.x + 1f, anchorRight.position.x + 5f);
+
+            float y;
+            do
+            {
+                y = Random.Range(anchorBotLeft.position.y, anchorLeft.position.y);
+
+            } while (SpawnPositionOnYAxis.FindAll(yy => Mathf.Abs(y - yy) < 1f).Count > 0); // avoid overlapping walls
+
+            BlobWall bw = Instantiate(blobWallPrefab, new Vector3(x, y, 0), Quaternion.identity).GetComponent<BlobWall>();
+            bw.side = s;
+            bw.lifeTime = Random.Range(wallLifeTimeMin, wallLifeTimeMax);
+
+            SpawnPositionOnYAxis.Add(bw.transform.position.y);
+
+            yield return new WaitForSeconds(Random.Range(spawnMinRate, spawnMaxRate));
+        }
+    }
+}
